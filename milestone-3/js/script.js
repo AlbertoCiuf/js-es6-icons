519c78b7d91d29c473fd19243251bc173d0ccf50
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2021-11-08 17:46:01 +0100

    milestone 3: done

--- milestone-3/js/script.js
+++ milestone-3/js/script.js
@@ -118,20 +118,43 @@ const row = document.createElement('div');
 row.className='row row-cols-5';
 container.append(row);
 
+//creo tre array che contengano solo determinati oggetti presenti nell'array originale in base al valore della key "type"
+const animalData = data.filter((obj) => obj.type==='animal');
+const vegetableData = data.filter((obj) => obj.type==='vegetable');
+const userData = data.filter((obj) => obj.type==='user');
 
-//main
-for (let index in data) {
-  let singleObject = data[index];
+const filter = document.getElementById('filter');
 
-  drawCards(singleObject);
-  chooseCardColor(singleObject);
+//MAIN
+//di default ed al refresh, la pagina carica le card dell'array che le contiene tutte (essendo anche selezionato di default "All" nel menù a tendina in html)
+createDOM(data);
 
+//event listener che cambia quali card visualizzare in base all'opzione scelta nel menù a tendina
+filter.addEventListener('change', (event)=> {
+  //assegno ad una variabile il valore dell'opzione scelta nel menù a tendina
+  let chosenFilter = parseInt(event.target.value);
+
+  //resetto l'innerHTML della row a cui vengono appese le card ad ogni cambiamento del valore selezionato
+  row.innerHTML='';
+
+  //assegno a selectedArray il risultato della funzione
+  const selectedArray = checkSelectedValue(chosenFilter);
+  
+  //come sopra, creo gli elementi html, ma stavolta in base all'array scelto in base all'opzione selezionata dal menù a tendina
+  createDOM(selectedArray)
+});
+
+
+//FUNZIONI
+
+//funzione che crea il contenuto html della pagina
+function createDOM(array) {
+  for (let index in array) {  
+    let singleObject = array[index];
+    drawCards(singleObject);
+    chooseCardColor(singleObject);
+  }
 }
-
-
-
-
-//funzioni
 
 //funzione che genera dinamicamente le card, utilizzando i valori delle chiavi dell'oggetto passato come parametro
 function drawCards(singObj) {
@@ -150,7 +173,6 @@ function drawCards(singObj) {
 
 }
 
-
 //funzione che colora le icone nelle card in base all'attributo corrispondente nell'oggetto.
 function chooseCardColor(singObj) {
   let icons = document.querySelectorAll('.card-icon > i');
@@ -160,4 +182,17 @@ function chooseCardColor(singObj) {
         icon.style.color=`${singObj.color}`
       }
     }
+}
+
+//funzione che verifica quale opzione è selezionata nel menù a tendina e di conseguenza da quale array andare a prendere gli elementi da visualizzare nelle card html
+function checkSelectedValue(chosenFilter) {
+  let array =[];
+  if (chosenFilter === 2) {
+    array = animalData;
+  } else if (chosenFilter === 3) {
+    array = vegetableData;
+  } else if (chosenFilter === 4) {
+    array = userData;
+  } else array = data;
+  return array
 }
