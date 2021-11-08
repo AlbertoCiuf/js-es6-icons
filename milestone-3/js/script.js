4d9cef0b1b23fc19cd7db894905ebd87dae5101a
AlbertoCiuf <89313766+AlbertoCiuf@users.noreply.github.com>
2021-11-08 16:47:50 +0100

    import file milestone 3

---
+++ milestone-3/js/script.js
@@ -0,0 +1,163 @@
+const data = [
+	{
+		name: 'cat',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'crow',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dog',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dove',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'dragon',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'horse',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'hippo',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'fish',
+		prefix: 'fa-',
+		type: 'animal',
+		family: 'fas',
+		color: 'orange'
+	},
+	{
+		name: 'carrot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'apple-alt',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'lemon',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'pepper-hot',
+		prefix: 'fa-',
+		type: 'vegetable',
+		family: 'fas',
+		color: 'green'
+	},
+	{
+		name: 'user-astronaut',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-graduate',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-ninja',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	},
+	{
+		name: 'user-secret',
+		prefix: 'fa-',
+		type: 'user',
+		family: 'fas',
+		color: 'blue'
+	}
+];
+
+const container = document.querySelector('main .container-fluid');
+const row = document.createElement('div');
+row.className='row row-cols-5';
+container.append(row);
+
+
+//main
+for (let index in data) {
+  let singleObject = data[index];
+
+  drawCards(singleObject);
+  chooseCardColor(singleObject);
+
+}
+
+
+
+
+//funzioni
+
+//funzione che genera dinamicamente le card, utilizzando i valori delle chiavi dell'oggetto passato come parametro
+function drawCards(singObj) {
+  const htmlCard = document.createElement('div');
+  htmlCard.className='ac-card';
+  htmlCard.innerHTML = 
+  `
+  <div class="card-icon">
+    <i class="${singObj.family} ${singObj.prefix}${singObj.name}"></i>
+  </div>
+  <div class="card-text text-uppercase">
+    ${singObj.name}
+  </div>
+  `;
+  row.append(htmlCard);
+
+}
+
+
+//funzione che colora le icone nelle card in base all'attributo corrispondente nell'oggetto.
+function chooseCardColor(singObj) {
+  let icons = document.querySelectorAll('.card-icon > i');
+    for (let icon of icons) {
+      //verifico che l'icona non abbia già un attributo "style" onde evitare che venga sovrascritto
+      if (icon.getAttribute('style')===null || icon.getAttribute('style') === "") {
+        icon.style.color=`${singObj.color}`
+      }
+    }
+}
